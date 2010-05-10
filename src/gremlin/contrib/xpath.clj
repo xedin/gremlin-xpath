680a4f25b1b2d781657c338eb49e0a303ddd83b8
Pavel Yaskevich <povel.y@gmail.com>
2010-05-11 02:50:00 +0300

    comment removed

--- src/gremlin/contrib/xpath.clj
+++ src/gremlin/contrib/xpath.clj
@@ -4,9 +4,3 @@
 (defn- parse-xpath-string [#^String xpath-line] ())
 
 (defn compile-xpath [#^String xpath-line] ())
-(comment
-  (ns gremlin.contrib.xpath)
-  (load "xpath")
-  (load-xml-document "examples/bookstore.xml")
-  (compile-xpath "/book/title[@lang = eng]")
-)
