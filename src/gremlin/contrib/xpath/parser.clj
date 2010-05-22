93ad2a7fb8e9865d88e6a4c002e2046a2709f074
Pavel Yaskevich <povel.y@gmail.com>
2010-05-23 01:18:39 +0300

    xpath - "./outE[@label = knows]/inV[@name != vadas]/@name" became possible.

--- src/gremlin/contrib/xpath/parser.clj
+++ src/gremlin/contrib/xpath/parser.clj
@@ -3,29 +3,57 @@
   (:import [com.tinkerpop.pipes.serial IdentityPipe Pipeline]
            [org.apache.commons.jxpath.ri Parser QName]
            [com.tinkerpop.pipes.serial.pgm EdgeVertexPipe VertexEdgePipe LabelFilterPipe PropertyFilterPipe PropertyPipe]
-           [org.apache.commons.jxpath.ri.compiler TreeCompiler LocationPath ExpressionPath ExtensionFunction Step NodeNameTest  NodeTypeTest ProcessingInstructionTest CoreOperationEqual]
+           [org.apache.commons.jxpath.ri.compiler TreeCompiler Path Expression Operation VariableReference Constant LocationPath ExpressionPath ExtensionFunction Step NodeNameTest  NodeTypeTest ProcessingInstructionTest CoreOperationEqual CoreOperationNotEqual]
            [gremlin.contrib.xpath.helpers PipesEnumHelper]
            [com.tinkerpop.blueprints.pgm.impls.tg TinkerGraphFactory]))
 
-;; declaring gremlin pipes pgm enums
+;; gremlin pipes pgm enums
 (defonce IN_VERTEX  (PipesEnumHelper/IN_VERTEX))
 (defonce OUT_VERTEX (PipesEnumHelper/OUT_VERTEX))
 
 (defonce IN_EDGES  (PipesEnumHelper/IN_EDGES))
 (defonce OUT_EDGES (PipesEnumHelper/OUT_EDGES))
 
+(defonce ALLOW (PipesEnumHelper/ALLOW))
+(defonce DISALLOW (PipesEnumHelper/DISALLOW))
+
 (defmulti step-token class)
 (defmulti analize-path class)
-(defmulti pipe-for-predicate class)
-
-(defmethod pipe-for-predicate CoreOperationEqual
-  [operation]
-  (seq operation))
-
-(defn- get-axis [step]
-  (->> step .getAxis (Step/axisToString)))
 
 (defmulti pipe-for-step get-axis)
+(defmulti pipe-for-predicate class)
+
+(defn- expr-type [expr]
+  (let [expr-class (.getClass expr)
+        superclass (.getSuperclass expr-class)]
+    (cond (= superclass Path) :path
+          (= superclass Operation) :operation
+          (= superclass Expression)
+            (cond (= expr-class Constant) :constant
+                  (= expr-class VariableReference) :variable)
+          :else (throw (Exception. "Could not determine expression type.")))))
+
+(defn pipe-for-operation-equal [arguments filter]
+  (let [operand-one (first arguments)
+        operand-two (last arguments)
+        operand-one-type (expr-type operand-one)
+        operand-two-type (expr-type operand-two)]
+    (cond (and (= operand-one-type :path) (= operand-two-type :constant))
+            (let [operand-one-step  (first (.getSteps operand-one))
+                  operand-one-axis  (get-axis operand-one-step)
+                  operand-one-token (step-token (.getNodeTest operand-one-step))
+                  operand-two-value (xpath-constant-value operand-two)]
+              (if (= operand-one-axis "attribute")
+                (if (= operand-one-token "label")
+                  (LabelFilterPipe. operand-two-value filter)
+                  (PropertyFilterPipe. operand-one-token operand-two-value filter))))
+          :else (throw (Exception. "not implemented yet.")))))
+
+(defmethod pipe-for-predicate CoreOperationEqual [operation]
+  (pipe-for-operation-equal (.getArguments operation) ALLOW))
+
+(defmethod pipe-for-predicate CoreOperationNotEqual [operation]
+  (pipe-for-operation-equal (.getArguments operation) DISALLOW))
 
 (defmethod pipe-for-step "self" [_]
   (IdentityPipe.))
@@ -44,23 +72,20 @@
 (defn- create-pipe [step]
   [(pipe-for-step step) (map pipe-for-predicate (seq (.getPredicates step)))])
 
-(defmethod analize-path LocationPath
-  [expr]
+(defmethod analize-path LocationPath [expr]
   (map #(create-pipe %) (seq (.getSteps expr))))
 
-(defmethod analize-path ExpressionPath
-  [expr]
+(defmethod analize-path ExpressionPath [expr]
   (println "expr path"))
 
-(defmethod analize-path ExtensionFunction
-  [expr]
+(defmethod analize-path ExtensionFunction [expr]
   (println "ext func"))
 
 (defmethod step-token NodeNameTest [node]
   (.. node getNodeName getName))
 
 (defmethod step-token NodeTypeTest [node]
-  (->> node .getNodeType (NodeTypeTest/nodeTypeToString )))
+  (->> node .getNodeType (NodeTypeTest/nodeTypeToString)))
 
 (defmethod step-token ProcessingInstructionTest [node]
   ())
@@ -71,5 +96,9 @@
 (defn parse [#^String xpath]
   (let [compiler (TreeCompiler.)
         path (Parser/parseExpression xpath compiler)
-        pipeline (analize-path path)] pipeline))
+        pipes (flatten (analize-path path))
+        pipeline (Pipeline. pipes)]
+    (.setStarts pipeline (.iterator (list vertex)))
+    (seq pipeline)))
+;;        pipeline (analize-path path)] pipeline))
 
--- src/gremlin/contrib/xpath/util.clj
+++ src/gremlin/contrib/xpath/util.clj
@@ -1,9 +1,25 @@
-(ns gremlin.contrib.xpath.util)
+(ns gremlin.contrib.xpath.util
+  (:import [org.apache.commons.jxpath.ri InfoSetUtil]
+           [org.apache.commons.jxpath.ri.compiler Step]))
+
+(defn get-axis [step]
+  (->> step .getAxis (Step/axisToString)))
+
+;; TODO: skip only first and last \' with regex
+(defn skip-quotes [#^String line]
+  (apply str (remove #(= \' %) (seq line))))
 
 (defn skip-spaces [#^String line]
-  (remove #(= \space %) (seq line)))
+  (apply str (remove #(= \space %) (seq line))))
 
 (defn flatten [x]
   (let [s? #(instance? clojure.lang.Sequential %)]
     (filter (complement s?) (tree-seq s? seq x))))
+
+(defn xpath-constant-value [constant]
+  (let [str-value (str constant)
+        num-value (InfoSetUtil/doubleValue str-value)]
+    (if (= (.isNaN num-value) true)
+      (skip-quotes str-value)
+      num-value)))
 
